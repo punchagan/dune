70e64963d70aa6e39a30ad7c7e53ab055f845a89
github-action-benchmark <github@users.noreply.github.com>
2025-01-13 06:33:56 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 53c6dfc3e86b863121d9e0da65d9b87a67130870

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736749886171,
+  "lastUpdate": 1736750036616,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -28911,6 +28911,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "21.684833111386666",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "53c6dfc3e86b863121d9e0da65d9b87a67130870",
+          "message": "refactor(pkg): use stdune in sat solver (#11282)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2025-01-12T17:03:38Z",
+          "tree_id": "432485e192b17f7aeda8b91a303622b539e0ed20",
+          "url": "https://github.com/punchagan/dune/commit/53c6dfc3e86b863121d9e0da65d9b87a67130870"
+        },
+        "date": 1736750035600,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "21.63473006098667",
             "unit": "seconds"
           }
         ]
