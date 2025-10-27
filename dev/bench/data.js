219ee29b50ed381d1ff13e6bcdb1d8067804be49
github-action-benchmark <github@users.noreply.github.com>
2025-10-27 06:56:55 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 686abc2473ca14708cfc8964610a11eff8aab30a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761548069108,
+  "lastUpdate": 1761548215121,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29201,6 +29201,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": 21.545931671793337,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "686abc2473ca14708cfc8964610a11eff8aab30a",
+          "message": "test(melange): improve `@all` alias test (#12627)\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2025-10-25T21:41:39Z",
+          "tree_id": "9d11c59f415248c1e2300e20777366fd60449404",
+          "url": "https://github.com/punchagan/dune/commit/686abc2473ca14708cfc8964610a11eff8aab30a"
+        },
+        "date": 1761548214430,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": 21.97967530968,
             "unit": "seconds"
           }
         ]
