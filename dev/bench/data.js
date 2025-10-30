2a10ff96fd4f80f8a3b12b7ad667ea7845ae79f6
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 02:14:35 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 821ae4f3ee51abab5df2397bd1e14348ce6e0af3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761790330685,
+  "lastUpdate": 1761790475678,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29288,6 +29288,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": 20.752773078426667,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "821ae4f3ee51abab5df2397bd1e14348ce6e0af3",
+          "message": "Merge pull request #12651 from Alizter/push-luppyxluzruz\n\ncram: rename conflict -> conflict_marker",
+          "timestamp": "2025-10-29T23:08:12Z",
+          "tree_id": "fcea2d2a97c9969dae62d974a59f504583c9b2b7",
+          "url": "https://github.com/punchagan/dune/commit/821ae4f3ee51abab5df2397bd1e14348ce6e0af3"
+        },
+        "date": 1761790474728,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": 21.62300485146,
             "unit": "seconds"
           }
         ]
