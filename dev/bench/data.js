e4664c30778abb66d2a80016625c72f6e3f748e7
github-action-benchmark <github@users.noreply.github.com>
2025-11-05 02:50:18 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for a6d78193c439e443776203c25014377dd35c2f86

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762310780839,
+  "lastUpdate": 1762311018662,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -73107,6 +73107,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": 1.0662426948333332,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6d78193c439e443776203c25014377dd35c2f86",
+          "message": "fix: -H for hidden modules (#12666)\n\n* fix: -H for hidden modules\n\nAlways make sure that hidden modules are available via -H when the compiler supports this -H flag.\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2025-11-04T22:20:37Z",
+          "tree_id": "9ac1026fadef56a96230ec6abe7882159efcc8f4",
+          "url": "https://github.com/punchagan/dune/commit/a6d78193c439e443776203c25014377dd35c2f86"
+        },
+        "date": 1762311017478,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": 46.531562567079995,
             "unit": "seconds"
           }
         ]
