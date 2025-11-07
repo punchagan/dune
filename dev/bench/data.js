8487f3429fb22784ee57d2ea0d7f10c8411ab4a9
github-action-benchmark <github@users.noreply.github.com>
2025-11-07 06:48:52 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 2346113ccf4b8f0c241b72b8a52c80829fcb4500

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762497981383,
+  "lastUpdate": 1762498131990,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -29404,6 +29404,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": 21.621922068586667,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2346113ccf4b8f0c241b72b8a52c80829fcb4500",
+          "message": "Merge pull request #12691 from Alizter/push-xvxvlytvxlnr\n\ndoc: (finally) add allow_empty to docs",
+          "timestamp": "2025-11-06T21:50:42Z",
+          "tree_id": "2906daf1f753aee8ade7d0cc2438c732d9c8efac",
+          "url": "https://github.com/punchagan/dune/commit/2346113ccf4b8f0c241b72b8a52c80829fcb4500"
+        },
+        "date": 1762498131009,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": 22.392052293779997,
             "unit": "seconds"
           }
         ]
