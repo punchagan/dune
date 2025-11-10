3c6a04df4c0abfbb65d5650f637cbc423b982753
github-action-benchmark <github@users.noreply.github.com>
2025-11-10 08:17:05 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 78236e057299303a1d8042d357abd34819ee1925

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762498384360,
+  "lastUpdate": 1762762625728,
   "repoUrl": "https://github.com/punchagan/dune",
   "entries": {
     "Melange Benchmark": [
@@ -94365,6 +94365,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": 16.146921109566666,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78236e057299303a1d8042d357abd34819ee1925",
+          "message": "Merge pull request #12686 from Alizter/push-kpxlutwryopo\n\nchore(nix): remove macos compat stubs",
+          "timestamp": "2025-11-07T10:09:45Z",
+          "tree_id": "3da79cb7a3ea48ea349f7b3a596a0a8643c08a59",
+          "url": "https://github.com/punchagan/dune/commit/78236e057299303a1d8042d357abd34819ee1925"
+        },
+        "date": 1762762615694,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": 16.096766975733335,
             "unit": "seconds"
           }
         ]
